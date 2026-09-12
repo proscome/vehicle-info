8f8c816e2cdf2d9702ee3c572c044df3695ba133
proscome <masakatsu.seko@gmail.com>
2026-09-13 08:00:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,125 @@
+const CACHE_NAME = "vehicle-info-pwa-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./vehicle.json"
+];
+
+
+/* --------------------------------
+ * インストール
+ * -------------------------------- */
+self.addEventListener(
+  "install",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(APP_FILES)
+        )
+        .then(() =>
+          self.skipWaiting()
+        )
+
+    );
+  }
+);
+
+
+/* --------------------------------
+ * 有効化
+ * -------------------------------- */
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys =>
+
+          Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          )
+
+        )
+        .then(() =>
+          self.clients.claim()
+        )
+
+    );
+  }
+);
+
+
+/* --------------------------------
+ * 通信
+ *
+ * 基本はネット優先。
+ * ネットに出られなければ
+ * キャッシュを使用。
+ * -------------------------------- */
+self.addEventListener(
+  "fetch",
+  event => {
+
+    if (
+      event.request.method !== "GET"
+    ) {
+      return;
+    }
+
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+
+          caches
+            .open(CACHE_NAME)
+            .then(cache => {
+
+              cache.put(
+                event.request,
+                copy
+              );
+
+            });
+
+
+          return response;
+
+        })
+
+        .catch(() => {
+
+          return caches.match(
+            event.request
+          );
+
+        })
+
+    );
+  }
+);
